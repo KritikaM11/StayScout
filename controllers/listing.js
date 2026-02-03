969ec54f68b8ec93ac7602b21e5dd39cf4b2a670
KritikaMittal <kmittal864@gmail.com>
2026-02-03 21:44:39 +0530

    trycatch

--- controllers/listing.js
+++ controllers/listing.js
@@ -44,95 +44,93 @@ export const add = async (req, res, next) => {
 
     // THIS IS WHERE YOU EXTRACT COORDINATES
     const geoURL = `https://nominatim.openstreetmap.org/search?format=json&q=${query}`;
-    try {
-        const response = await fetch(geoURL, {
-            headers: {
-                "User-Agent": "StayScout/1.0" // Use your project name
-            }
-        });
-        if (!response.ok) {
-            throw new Error(`Nominatim API Error: ${response.statusText}`);
+    const response = await fetch(geoURL, {
+        headers: {
+            "User-Agent": "StayScout/1.0 (student project)"
         }
-        const data = await response.json();
-        if (!data.length) {
-            req.flash("error", "Location not found. Please enter a valid place.");
-            return res.redirect("/listings/new");
-        }
-
-        listing.geometry = {
-            type: "Point",
-            coordinates: [
-                parseFloat(data[0].lon),
-                parseFloat(data[0].lat)
-            ]
-        };
-
-        let url = req.file.path;
-        let filename = req.file.filename;
-        listing.owner = req.user._id;
-        listing.image = { url, filename };
-
-        let savedListing = await listing.save();
-
-        console.log(savedListing);
-        req.flash("success", "New Listing created!");
-        res.redirect("/listings");
+    });
+    const data = await response.json();
+    if (!data.length) {
+        req.flash("error", "Location not found. Please enter a valid place.");
+        return res.redirect("/listings/new");
     }
+
+    listing.geometry = {
+        type: "Point",
+        coordinates: [
+            parseFloat(data[0].lon),
+            parseFloat(data[0].lat)
+        ]
+    };
+
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.owner = req.user._id;
+    listing.image = { url, filename };
+
+    // let category = req.body.category;
+    // listing.category = category;
+
+    let savedListing = await listing.save();
+
+    console.log(savedListing);
+    req.flash("success", "New Listing created!");
+    res.redirect("/listings");
 }
 
 export const show = async (req, res) => {
-        const { id } = req.params;
-        const listing = await Listing.findById(id)
-            .populate({
-                path: "reviews",
-                populate: {
-                    path: "author"
-                }
-            })
-            .populate("owner");
+    const { id } = req.params;
+    const listing = await Listing.findById(id)
+        .populate({
+            path: "reviews",
+            populate: {
+                path: "author"
+            }
+        })
+        .populate("owner");
 
-        if (!listing) {
-            req.flash("error", "Listing you requested for does not exist!");
-            res.redirect("/listings");
-        }
-        res.render("listings/show", { listing });
-    }
-
-    export const edit = async (req, res) => {
-        let { id } = req.params;
-        let listing = await Listing.findById(id);
-        if (!listing) {
-            req.flash("error", "Listing you requested for does not exist!");
-            res.redirect("/listings");
-        }
-
-        let originalimgurl = listing.image.url;
-        console.log(listing.image.url);
-        originalimgurl = originalimgurl.replace(
-            "/upload",
-            "/upload/w_300"
-        );
-
-        res.render("listings/edit", { listing, originalimgurl });
-    }
-
-    export const update = async (req, res) => {
-        const { id } = req.params;
-        let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-        if (typeof req.file != "undefined") {
-            let url = req.file.path;
-            let filename = req.file.filename;
-            listing.image = { url, filename };
-            await listing.save();
-        }
-        req.flash("success", "Listing Updated!");
-        res.redirect(`/listings/${id}`);
-    }
-
-    export const deletecontroller = async (req, res) => {
-        const { id } = req.params;
-        const deletedListings = await Listing.findOneAndDelete({ _id: id });
-        console.log(deletedListings);
-        req.flash("success", "Listing Deleted!");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
         res.redirect("/listings");
     }
+    res.render("listings/show", { listing });
+}
+
+export const edit = async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
+
+    let originalimgurl = listing.image.url;
+    console.log(listing.image.url);
+    originalimgurl = originalimgurl.replace(
+        "/upload",
+        "/upload/w_300"
+    );
+
+    res.render("listings/edit", { listing, originalimgurl });
+}
+
+export const update = async (req, res) => {
+    const { id } = req.params;
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    if (typeof req.file != "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+}
+
+export const deletecontroller = async (req, res) => {
+    const { id } = req.params;
+    const deletedListings = await Listing.findOneAndDelete({ _id: id });
+    console.log(deletedListings);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+}
